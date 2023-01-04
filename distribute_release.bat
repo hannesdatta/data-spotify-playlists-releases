25eded36fa8e3bb4c3b596b733c3e3132b423745
Hannes Datta <h.datta@tilburguniversity.edu>
2023-01-04 14:07:17 +0100

    distribute new promotion release w/ positions

--- distribute_release.bat
+++ distribute_release.bat
@@ -1,1 +1,1 @@
-aws s3 sync release/. s3://uvt-data-music-streaming/everynoise/releases --region eu-central-1  --exclude "*.*" --include "*.csv" 
+aws s3 sync release/2023_01_04/. s3://uvt-data-music-streaming/everynoise/releases/2023_01_04 --region eu-central-1  --exclude "*.*" --include "*.csv" 
